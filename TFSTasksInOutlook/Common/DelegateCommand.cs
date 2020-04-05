30589f6fb079e686d36458d2cb54d30b8112720d
Joy George Kunjikkuru <joymon@gmail.com>
2020-04-05 17:43:34 -0400

    Enabled all warning to erros

--- TFSTasksInOutlook/Common/DelegateCommand.cs
+++ TFSTasksInOutlook/Common/DelegateCommand.cs
@@ -1,27 +1,33 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows.Input;
 
 namespace TFSTasksInOutlook.Common
-  {
-  internal class DelegateCommand : ICommand
+{
+    internal class DelegateCommand : ICommand
     {
-    readonly Predicate<object> _canExecute;
-    readonly Action<object> _execute;
-    public DelegateCommand(Predicate<object> canexecute, Action<object> execute)
-      : this()
-      {
-      _canExecute = canexecute;
-      _execute = execute;
-      }
-    public DelegateCommand() { }
-    public bool CanExecute(object parameter)
-      {
-      return _canExecute == null || _canExecute(parameter);
-      }
-    public event EventHandler CanExecuteChanged;
-    public void Execute(object parameter)
-      {
-      _execute(parameter);
-      }
+        readonly Predicate<object> _canExecute;
+        readonly Action<object> _execute;
+        public DelegateCommand(Predicate<object> canexecute, Action<object> execute)
+          : this()
+        {
+            _canExecute = canexecute;
+            _execute = execute;
+        }
+        public DelegateCommand() { }
+        bool ICommand.CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+        [SuppressMessage("Build","CS0067")]
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        void ICommand.Execute(object parameter)
+        {
+            _execute(parameter);
+        }
     }
-  }
+}
